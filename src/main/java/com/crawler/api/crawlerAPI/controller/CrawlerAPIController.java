1e5b54cf2479ab865cf1eb6ca9d0da9b4e14cdee
ludvig <ludvig.genehr@kestraa.com>
2018-12-25 19:44:07 -0200

    Ajustes

--- src/main/java/com/crawler/api/crawlerAPI/controller/CrawlerAPIController.java
+++ src/main/java/com/crawler/api/crawlerAPI/controller/CrawlerAPIController.java
@@ -8,14 +8,12 @@ import com.crawler.api.crawlerAPI.mapper.totalPostsOrderByHour;
 import com.crawler.api.crawlerAPI.mapper.userWithMoreFollowers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/list")
-public class ListController {
+public class CrawlerAPIController {
 
     @Autowired
     usersOrderByTopFollowersRepository usersOrderByTopFollowersRepo;
@@ -26,17 +24,17 @@ public class ListController {
     @Autowired
     totalPostsByTagAndCountryRepository totalPostsByTagAndCountryRepo;
 
-    @GetMapping("/AllUsersOrderByTopFollowers")
+    @GetMapping("/list/AllUsersOrderByTopFollowers")
     public List<userWithMoreFollowers> listAllUsersOrderByTopFollowers() {
         return usersOrderByTopFollowersRepo.listAllUsersOrderByTopFollowers();
     }
 
-    @GetMapping("/TotalPostsOrderByHour")
+    @GetMapping("/list/TotalPostsOrderByHour")
     public List<totalPostsOrderByHour> listTotalPostsOrderByHour() {
         return totalPostsOrderByHourRepo.listTotalPostsOrderByHour();
     }
 
-    @GetMapping("/TotalPostsByTagAndCountry")
+    @GetMapping("/list/TotalPostsByTagAndCountry")
     public List<totalPostsByTagAndCountry> listTotalPostsByTagAndCountry() {
         return totalPostsByTagAndCountryRepo.listTotalPostsByTagAndCountry();
     }
--- src/main/java/com/crawler/api/crawlerAPI/main/CrawlerApiApplication.java
+++ src/main/java/com/crawler/api/crawlerAPI/main/CrawlerApiApplication.java
@@ -1,18 +1,22 @@
-package com.crawler.api.crawlerAPI.web;
+package com.crawler.api.crawlerAPI.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+//@EnableOAuth2Sso
 @SpringBootApplication
 @ComponentScan("com.crawler.api.crawlerAPI.controller")
 @EnableMongoRepositories("com.crawler.api.crawlerAPI")
 public class CrawlerApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrawlerApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerApiApplication.class, args);
+    }
+
 }
+
+
 
 
